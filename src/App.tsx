02c8e8f505a1fd7f1e4e2d55b42dd4538aa49502
Kim-HyounSuk <kkoor586@naver.com>
2023-10-07 18:38:49 +0900

    -: react-query 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { ThemeProvider } from 'styled-components';
 import { Theme, GlobalStyle } from '@/styles';
 import { Layout } from '@/components';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <ThemeProvider theme={Theme}>
-      <GlobalStyle />
-      <Layout />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+        <Layout />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 

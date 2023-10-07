1b0861b18aef030bcb709c2e6691633c7b5b8af1
Kim-HyounSuk <kkoor586@naver.com>
2023-10-07 18:47:31 +0900

    -: react-query 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import { Theme, GlobalStyle } from '@/styles';
 import { Layout } from '@/components';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={Theme}>
-        <GlobalStyle />
-        <Layout />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <Layout />
+    </ThemeProvider>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from '@/router';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );

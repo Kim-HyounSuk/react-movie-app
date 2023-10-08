4759d8a946a48429234e861886d13a5f2e914e70
Kim-HyounSuk <kkoor586@naver.com>
2023-10-09 04:31:46 +0900

    Feat: Footer 컴포넌트

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,24 @@
+import styled from 'styled-components';
+
 const Footer = () => {
-  return <></>;
+  return (
+    <Container>
+      <Text>© 2023 Vanlan. All rights reserved.</Text>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+`;
+
+const Text = styled.span`
+  font-size: 1rem;
+  font-weight: 600;
+  text-align: center;
+`;
 
 export default Footer;
